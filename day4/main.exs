2f19168c1aa62ac40b1f5541cc77817a7f6f52f1
Hubert Kasprzycki <67634024+hhubert6@users.noreply.github.com>
2024-12-13 22:44:40 +0100

    Done day4 part 2

--- day4/main.exs
+++ day4/main.exs
@@ -1,5 +1,5 @@
 defmodule Day4 do
-  def search(filename) do
+  def part_one(filename) do
     data =
       File.read!(filename)
       |> String.split("\n", trim: true)
@@ -13,6 +13,22 @@ defmodule Day4 do
 
     Map.keys(data)
     |> Enum.reduce(0, &(&2 + check(data, &1)))
+  end
+
+  def part_two(filename) do
+    data =
+      File.read!(filename)
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.split(&1, "", trim: true))
+
+    data =
+      for {row, y} <- Enum.with_index(data),
+          {char, x} <- Enum.with_index(row),
+          into: %{},
+          do: {{y, x}, char}
+
+    Map.keys(data)
+    |> Enum.count(&(data[&1] == "A" and check_X(data, &1)))
   end
 
   defp check(data, pos) do
@@ -32,7 +48,22 @@ defmodule Day4 do
 
   defp gen_directions(),
     do: for(dx <- -1..1, dy <- -1..1, {dx, dy} != {0, 0}, do: {dx, dy})
+
+  defp check_X(data, {y, x}) do
+    s1 =
+      [data[{y - 1, x - 1}], data[{y, x}], data[{y + 1, x + 1}]]
+      |> Enum.join()
+
+    s2 =
+      [data[{y + 1, x - 1}], data[{y, x}], data[{y - 1, x + 1}]]
+      |> Enum.join()
+
+    (s1 == "MAS" or s1 == "SAM") and (s2 == "SAM" or s2 == "MAS")
+  end
 end
 
-Day4.search("input.txt")
+Day4.part_one("input.txt")
+|> IO.inspect()
+
+Day4.part_two("input.txt")
 |> IO.inspect()
