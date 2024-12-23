297b14de4cee904cf4e38a359f19679c528bf7ae
Hubert Kasprzycki <67634024+hhubert6@users.noreply.github.com>
2024-12-23 20:00:05 +0100

    Done day5 part 2

--- day5/main.exs
+++ day5/main.exs
@@ -15,6 +15,48 @@ defmodule Day5 do
     |> Enum.sum()
   end
 
+  def part_two(filename) do
+    [rules, updates] =
+      File.read!(filename)
+      |> String.split("\n\n")
+      |> Enum.map(&String.split(&1, "\n", trim: true))
+
+    rules = parse_rules(rules)
+
+    updates
+    |> Stream.map(&parse_update/1)
+    |> Stream.filter(&(not validate_update(&1, rules)))
+    |> correct_order(rules)
+    |> Stream.map(&:array.get(div(:array.size(&1), 2), &1))
+    |> Stream.map(&String.to_integer/1)
+    |> Enum.sum()
+  end
+
+  defp correct_order(updates, rules) do
+    updates
+    |> Stream.map(&(Enum.map(&1, fn {page, _} -> page end) |> :array.from_list()))
+    |> Stream.map(fn report ->
+      for i <- 1..(:array.size(report) - 1), reduce: report do
+        acc ->
+          pages_after = Map.get(rules, :array.get(i, acc), MapSet.new())
+
+          for j <- 0..(i - 1), reduce: acc do
+            acc2 ->
+              if(MapSet.member?(pages_after, :array.get(j, acc2))) do
+                swap(acc2, i, j)
+              else
+                acc2
+              end
+          end
+      end
+    end)
+  end
+
+  defp swap(array, i, j) do
+    arr2 = :array.set(i, :array.get(j, array), array)
+    :array.set(j, :array.get(i, array), arr2)
+  end
+
   defp parse_rules(rules) do
     rules
     |> Stream.map(&String.split(&1, "|"))
@@ -45,4 +87,7 @@ defmodule Day5 do
 end
 
 Day5.part_one("input.txt")
+|> IO.inspect()
+
+Day5.part_two("input.txt")
 |> IO.inspect()
